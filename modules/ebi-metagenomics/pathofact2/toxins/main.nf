3d07fa6256d353c7a69213d873b58a0b92240738
Ales-ibt <ales@ebi.ac.uk>
2026-01-22 15:47:40 +0000

    toxins module in place

---
+++ modules/ebi-metagenomics/pathofact2/toxins/main.nf
@@ -0,0 +1,48 @@
+process PATHOFACT2_TOXINS {
+    tag "$meta.id"
+    label 'process_medium'
+
+    conda "${moduleDir}/environment.yml"
+    container "quay.io/microbiome-informatics/pathofact2_env:v1.0.2"
+
+    input:
+    tuple val(meta), path(fasta)
+
+    output:
+    tuple val(meta), path("*_classifier_toxins_filtered.tsv"), emit: tsv
+    tuple val("${task.process}"), val('python'), eval("python --version | sed 's/Python //g'"), topic: versions, emit: versions_python
+
+    when:
+    task.ext.when == null || task.ext.when
+
+    script:
+    def args = task.ext.args ?: ''               // This customizes the command: toxins prediction
+    def args2 = task.ext.args2 ?: 'thr=0.6'      // This customizes the command: awk filtering
+    def prefix = task.ext.prefix ?: "${meta.id}"
+    def is_compressed = fasta.getExtension() == "gz"
+    def fasta_name = is_compressed ? fasta.getBaseName() : fasta
+    def uncompress_input = is_compressed ? "gzip -c -d ${fasta} > ${fasta_name}" : ''
+    """
+    $uncompress_input
+
+    tox_predict.py \\
+        ${args} \\
+        -s ${fasta_name} \\
+        -m ${pathofact_db}/TOX/final_model.joblib \\
+        -v ${pathofact_db}/TOX/ \\
+        --cpus ${task.cpus} \\
+        -o ${prefix}_classifier_toxins.tsv
+
+    awk -v ${args2} -F'\\t' 'NR==1 || \$3 > thr { print }' ${prefix}_classifier_toxins.tsv > ${prefix}_classifier_toxins_filtered.tsv
+    """
+
+    stub:
+    def args = task.ext.args ?: ''
+    def args2 = task.ext.args2 ?: ''
+    def prefix = task.ext.prefix ?: "${meta.id}"
+    """
+    echo $args
+    echo $args2
+    touch ${prefix}_classifier_toxins_filtered.tsv
+    """
+}
---
+++ modules/ebi-metagenomics/pathofact2/toxins/resources/usr/bin/tox_predict.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+# Copyright 2024-2026 EMBL - European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.#!/usr/bin/env python
+# AUTHOR: Luis F. Delgado (lfdelzam@hotmail.com)
+
+import argparse
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.sparse import hstack
+from joblib import Parallel, delayed
+import os
+
+# Function to read sequences and their IDs from a FASTA file
+def read_fasta_sequences(file_path):
+    sequences = []
+    with open(file_path, "r") as fin:
+        seq_id = None
+        seq = ''
+        for line in fin:
+            line = line.rstrip()
+            if line.startswith(">"):
+                if seq_id and seq:
+                    sequences.append((seq_id, seq))
+                seq_id = line.split()[0][1:]  # Remove the '>' character to get the ID
+                seq = ''
+            else:
+                seq += line
+        if seq_id and seq:
+            sequences.append((seq_id, seq))  # Append the last sequence
+    return sequences
+
+# Function to preprocess sequences into k-mer features using loaded vectorizers
+def preprocess_sequences(sequences, kmer_sizes, vectorizer_dir, num_cpus):
+    def process_kmer(k):
+        vectorizer_path = os.path.join(vectorizer_dir, f'vectorizer_{k}.pkl')
+        vectorizer = joblib.load(vectorizer_path)  # Load the saved vectorizer
+        seqs = [seq for _, seq in sequences]  # Extract only the sequences for vectorization
+        X_kmer = vectorizer.transform(seqs)
+        return X_kmer
+
+    # Process k-mer features in parallel
+    X_kmers = Parallel(n_jobs=num_cpus)(delayed(process_kmer)(k) for k in kmer_sizes)
+    X_combined = hstack(X_kmers)
+    return X_combined
+
+# Main function to handle argument parsing and workflow execution
+def main():
+    parser = argparse.ArgumentParser(description='Predict sequences using a pre-trained model.')
+    parser.add_argument('-m', '--model', type=str, required=True, help='Path to the saved model (final_model.joblib)')
+    parser.add_argument('-s', '--sequences', type=str, required=True, help='Path to the FASTA file containing new sequences')
+    parser.add_argument('-k', '--kmers', type=int, nargs='+', default=[5], help='List of k-mer sizes used during training')
+    parser.add_argument('-v', '--vectorizer_dir', type=str, required=True, help='Directory containing the vectorizer files')
+    parser.add_argument('-o', '--output', type=str, required=True, help='Path to the output file to save predictions')
+    parser.add_argument('--cut_off', type=str, default="0", help='probaility cut_off to be cosidered as positive classification')
+    parser.add_argument('-cpu', '--cpus', type=int, default=-1, help='Number of CPUs to use for parallel processing. Default is -1 (use all available CPUs)')
+
+    args = parser.parse_args()
+
+    num_cpus = args.cpus if args.cpus > 0 else -1  # Use all available CPUs if non-positive value is provided
+
+    # Step 1: Load the saved model
+    best_model = joblib.load(args.model)
+
+    # Step 2: Load new sequences for prediction
+    sequences_with_ids = read_fasta_sequences(args.sequences)
+
+    # Step 3: Preprocess new sequences using the saved vectorizers
+    X_new = preprocess_sequences(sequences_with_ids, args.kmers, args.vectorizer_dir, num_cpus)
+
+    #print(best_model.get_params())
+    # Step 4: Predict using the loaded model
+    predictions = best_model.predict(X_new)
+    probabilities = best_model.predict_proba(X_new)
+
+    # Step 5: Output the predictions to a file
+    with open(args.output, 'w') as fout:
+        fout.write(f"Sequence\tPrediction\tProbability\n")
+        for i, (seq_id, _) in enumerate(sequences_with_ids):
+            prob=probabilities[i][1]
+            pred=predictions[i]
+            if prob >= float(args.cut_off):
+                fout.write(f"{seq_id}\t{pred}\t{prob:.2f}\n")
+    
+if __name__ == "__main__":
+    main()
