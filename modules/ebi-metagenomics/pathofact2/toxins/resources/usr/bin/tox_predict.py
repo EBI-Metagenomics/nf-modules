ea92dce8dff4adbff649a8448cd9b2b964b36052
Alejandra Escobar <ales@ebi.ac.uk>
2026-01-23 13:06:05 +0000

    Fix license comment formatting in tox_predict.py

--- modules/ebi-metagenomics/pathofact2/toxins/resources/usr/bin/tox_predict.py
+++ modules/ebi-metagenomics/pathofact2/toxins/resources/usr/bin/tox_predict.py
@@ -11,7 +11,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.#!/usr/bin/env python
+# limitations under the License.
 # AUTHOR: Luis F. Delgado (lfdelzam@hotmail.com)
 
 import argparse
